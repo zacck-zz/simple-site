d721196aa48fadc1c67cbb49dc349c7cc4ec1a7d
Zacck <zacck@Zaccks-MacBook-Pro.local>
2016-08-22 08:55:07 +0200

    added Nav bar and associated tests

---
+++ app/components/Nav.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Nav = React.createClass({
+    render: function() {
+       return(
+               <div>
+                <p>Nav  Component</p>
+               </div>
+             );
+    }
+});
+module.exports =  Nav;
---
+++ app/tests/components/Nav.jsx
@@ -0,0 +1,20 @@
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils')
+
+//actual component we need to render
+var Nav = require('Nav');
+
+//Perform assertions
+describe('Nav', () =>{
+
+it('should exist', () => {
+   expect(Nav).toExist();
+});
+
+//other tests
+
+});
